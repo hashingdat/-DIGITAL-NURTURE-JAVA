3ee3168cf5e532a285b990bde455226a78d1f25a
Harsh <harshhhhh13@example.com>
2026-07-16 15:32:12 +0530

    week 3 commit

--- week3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ week3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,9 +1,35 @@
+//package com.cognizant.spring_learn;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//@SpringBootApplication
+//public class SpringLearnApplication {
+//
+//	private static final Logger LOGGER =
+//			LoggerFactory.getLogger(SpringLearnApplication.class);
+//
+//	public static void main(String[] args) {
+//
+//		SpringApplication.run(SpringLearnApplication.class, args);
+//
+//		LOGGER.info("Inside Main");
+//	}
+//}
+
+
 package com.cognizant.spring_learn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -15,6 +41,22 @@ public class SpringLearnApplication {
 
 		SpringApplication.run(SpringLearnApplication.class, args);
 
-		LOGGER.info("Inside Main");
+		displayCountry();
 	}
+
+	public static void displayCountry() {
+
+		LOGGER.debug("Start");
+
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("country.xml");
+
+		Country country =
+				context.getBean("country", Country.class);
+
+		LOGGER.debug("Country : {}", country);
+
+		LOGGER.debug("End");
+	}
+
 }
