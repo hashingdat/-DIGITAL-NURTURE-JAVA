d37ff1d9ddc8b081646f2019ceffc38a9b12d545
Harsh <harshhhhh13@example.com>
2026-07-25 03:51:07 +0530

    Fix: structure of files

--- src/src/week1/designAndPrinciples/factorymethod/WordDocument.java
+++
@@ -1,8 +0,0 @@
-package week1.designAndPrinciples.factorymethod;
-
-public class WordDocument implements Document {
-    @Override
-    public void open() {
-        System.out.println("Openning WordDocument");
-    }
-}
