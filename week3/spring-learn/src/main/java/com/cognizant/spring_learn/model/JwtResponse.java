751202ac1b91f7527af044fbe1b781be41266a76
Harsh <harshhhhh13@example.com>
2026-07-24 11:25:10 +0530

    week 4 commit

---
+++ week3/spring-learn/src/main/java/com/cognizant/spring_learn/model/JwtResponse.java
@@ -0,0 +1,22 @@
+//package com.cognizant.spring_learn.model;
+//
+//public class Jwtresponse {
+//}
+package com.cognizant.spring_learn.model;
+
+public class JwtResponse {
+
+    private String token;
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
